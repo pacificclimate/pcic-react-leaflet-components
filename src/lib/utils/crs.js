d63579df6790d24a9d7406c73245bafaeb5e4551
rod-glover <rod.glover.ca@gmail.com>
2021-04-13 17:05:42 -0700

    Improve documentation

--- src/lib/utils/crs.js
+++ src/lib/utils/crs.js
@@ -11,6 +11,13 @@ export const pixelWidth = 0.00028;
 // The user must supply one of `scaleDenominator` or
 // `metersPerUnit`, `tileMatrixMinX`, `tileMatrixMaxX`, and `tileWidth` or
 // `metersPerUnit`, `tileMatrixMinY`, `tileMatrixMaxY`, and `tileHeight`.
+// The required options for this are `resolutions` and `origin`.
+//
+// Once we have the value for the zoom-0 `scaleDenominator`, it is
+// straightforward to compute `resolutions`. If not directly available, the
+// value can be derived, in our particular case from fundamental information
+// about the tileset, namely its extents or bounds. (For details, see
+// https://github.com/pacificclimate/pcic-react-leaflet-components/issues/1#issuecomment-818194664)
 export function resolutions(
   {
     metersPerUnit,
@@ -40,6 +47,11 @@ export function resolutions(
 
 
 // Compute options for `L.Proj.CRS` from CRS info and tile matrix parameters.
+//
+// See comments to `resolutions` re. resolution computations.
+//
+// The values for `origin` and `bounds` are also derived from the extents. (See
+// https://github.com/pacificclimate/pcic-react-leaflet-components/issues/1#issuecomment-818231259)
 export function projCRSOptions(tileMatrixParams) {
   const {tileMatrixMinX, tileMatrixMinY, tileMatrixMaxX, tileMatrixMaxY} =
     tileMatrixParams;
