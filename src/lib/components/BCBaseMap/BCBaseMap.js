209d98c7142c4b2db1cea9ff3467d0bff46d3eb3
rod-glover <rod.glover.ca@gmail.com>
2021-08-24 13:33:23 -0700

    Use BaseMap for BCBaseMap

--- src/lib/components/BCBaseMap/BCBaseMap.js
+++ src/lib/components/BCBaseMap/BCBaseMap.js
@@ -1,43 +1,15 @@
-// BCBaseMap: Component that establishes a map of B.C., and nothing more.
+// BCBaseMap: Component that establishes a map of B.C.,
+// in BC Albers projection, and nothing more.
 
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-
-import { Map, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
 
 import 'proj4';
 import 'proj4leaflet';
 import 'leaflet/dist/leaflet.css';
 
-import './BCBaseMap.css';
-import { projCRSOptions } from '../../utils/crs';
+import BaseMap from '../BaseMap';
 
-// Set up BC Albers projection
-
-const numResolutions = 12;
-
-// Create Leaflet CRS object
-const bcAlbersCrs = new L.Proj.CRS(
-  'EPSG:3005',
-  '+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs',
-  projCRSOptions({
-    // From the definition of the projection (SRS)
-    metersPerUnit: 1,  // Proj.4: +units=m
-
-    // From tile mill
-    // For some reason, `scaleDenominator` is only half what we would expect it to
-    // be. We specify it here to override the extents-based resolution
-    // computations.
-    scaleDenominator: 27901785.714285714,
-    tileMatrixMinX: -1000000,
-    tileMatrixMaxX: 3000000,
-    tileWidth: 256,
-    tileMatrixMinY: -1000000,
-    tileMatrixMaxY: 3000000,
-    numResolutions,
-  })
-);
 
 export default class BCBaseMap extends PureComponent {
   static propTypes = {
@@ -53,6 +25,31 @@ export default class BCBaseMap extends PureComponent {
     mapRef: (() => null),
   };
 
+  static tileset = {
+    url: process.env.REACT_APP_BC_ALBERS_URL + '/1.0.0/bc_osm/{z}/{x}/{y}.png',
+    projection: {
+      code: 'EPSG:3005',
+      proj4def: '+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs',
+    },
+    tileMatrix: {
+      // From the definition of the projection (SRS)
+      metersPerUnit: 1,  // Proj.4: +units=m
+
+      // From tile generation
+      // For some reason, `scaleDenominator` is only half what we would expect it to
+      // be. We specify it here to override the extents-based resolution
+      // computations.
+      scaleDenominator: 27901785.714285714,
+      tileMatrixMinX: -1000000,
+      tileMatrixMaxX: 3000000,
+      tileWidth: 256,
+      tileMatrixMinY: -1000000,
+      tileMatrixMaxY: 3000000,
+      numResolutions: 12,
+    },
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+  };
+
   static initialViewport = {
     center: {
       lat: 55.0,
@@ -64,22 +61,13 @@ export default class BCBaseMap extends PureComponent {
   render() {
     const { mapRef, children, ...rest } = this.props;
     return (
-      <Map
-        crs={bcAlbersCrs}
-        minZoom={0}
-        maxZoom={numResolutions}
+      <BaseMap
+        tileset={BCBaseMap.tileset}
         ref={mapRef}
         {...rest}
       >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url={process.env.REACT_APP_BC_ALBERS_URL + '/1.0.0/bc_osm/{z}/{x}/{y}.png'}
-          subdomains={'abc'}
-          noWrap={true}
-          maxZoom={numResolutions}
-        />
         {children}
-      </Map>
+      </BaseMap>
     );
   }
 }
