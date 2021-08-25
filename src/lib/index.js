cf0b2b66be261550a602b5db66e0b72d9a83690c
rod-glover <rod.glover.ca@gmail.com>
2021-08-25 16:17:37 -0700

    Update index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,3 @@
 export { default as BCBaseMap } from './components/BCBaseMap';
+export { default as BCBaseMapDeprecated } from './components/BCBaseMapDeprecated';
 export { default as YNWTBaseMap } from './components/YNWTBaseMap';
