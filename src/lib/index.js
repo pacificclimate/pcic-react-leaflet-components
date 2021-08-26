b6a403398b6aea03922a839de1cf6d5480b56385
rod-glover <rod.glover.ca@gmail.com>
2021-08-26 09:22:17 -0700

    Export StaticControl

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,4 @@
 export { default as BCBaseMap } from './components/BCBaseMap';
 export { default as BCBaseMapDeprecated } from './components/BCBaseMapDeprecated';
 export { default as YNWTBaseMap } from './components/YNWTBaseMap';
+export { default as StaticControl } from './components/StaticControl';
