5bdd187107830054b69bc18216a376acb7f58a76
rod-glover <rod.glover.ca@gmail.com>
2021-11-03 15:19:28 -0700

    Convert DemoBaseMap to hooks

--- playground/src/DemoBaseMap/DemoBaseMap.js
+++ playground/src/DemoBaseMap/DemoBaseMap.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { CircleMarker, LayerGroup, Popup } from 'react-leaflet';
 import { range, map } from 'lodash/fp';
@@ -7,68 +7,64 @@ import { range, map } from 'lodash/fp';
 import './DemoBaseMap.css'
 
 
-export default class DemoBaseMap extends React.Component {
-  static propTypes = {
-    BaseMap: PropTypes.object.isRequired,
-    initialViewport: PropTypes.object.isRequired,
-    markers: PropTypes.array,
-    numMaps: PropTypes.number,
-  };
+function DemoBaseMap({ BaseMap, initialViewport, markers, numMaps}) {
+  const [viewport, setViewport] = useState(initialViewport);
 
-  static defaultProps = {
-    numMaps: 1,
-    markers: [],
-  };
-
-  state = {
-    viewport: this.props.initialViewport,
-  };
-
-  handleViewportChange = viewport => this.setState({ viewport });
-
-  render() {
-    const { BaseMap, markers, numMaps } = this.props;
-    const colWidth = Math.floor(12 / numMaps);
-    return (
-      <Container fluid>
-        <Row>
-          <Col xs={12}>
-            <h2>{this.props.numMaps} synchronized basemaps</h2>
-            <p>viewport: {JSON.stringify(this.state.viewport)}</p>
-          </Col>
-        </Row>
-        <Row>
-          {
-            map(i => (
-              <Col key={i} xs={colWidth}>
-                <BaseMap
-                  viewport={this.state.viewport}
-                  onViewportChange={this.handleViewportChange}
-                >
-                  {/*<LayerGroup>*/}
-                  {/*  {*/}
-                  {/*    markers.map(*/}
-                  {/*      ({ lng, lat, comment }, i) => (*/}
-                  {/*        <CircleMarker*/}
-                  {/*          key={i}*/}
-                  {/*          center={{ lng, lat }}*/}
-                  {/*          radius={5}*/}
-                  {/*          color={'#0000FF'}*/}
-                  {/*        >*/}
-                  {/*          <Popup>*/}
-                  {/*            Lng: {lng}, Lat: {lat} <br/> {comment}*/}
-                  {/*          </Popup>*/}
-                  {/*        </CircleMarker>*/}
-                  {/*      )*/}
-                  {/*    )*/}
-                  {/*  }*/}
-                  {/*</LayerGroup>*/}
-                </BaseMap>
-              </Col>
-            ))(range(0,this.props.numMaps))
-          }
-        </Row>
-      </Container>
-    );
-  }
+  const colWidth = Math.floor(12 / numMaps);
+  return (
+    <Container fluid>
+      <Row>
+        <Col xs={12}>
+          <h2>{numMaps} synchronized basemaps</h2>
+          <p>viewport: {JSON.stringify(viewport)}</p>
+        </Col>
+      </Row>
+      <Row>
+        {
+          map(i => (
+            <Col key={i} xs={colWidth}>
+              <BaseMap
+                viewport={viewport}
+                onViewportChange={setViewport}
+              >
+                {/*<LayerGroup>*/}
+                {/*  {*/}
+                {/*    markers.map(*/}
+                {/*      ({ lng, lat, comment }, i) => (*/}
+                {/*        <CircleMarker*/}
+                {/*          key={i}*/}
+                {/*          center={{ lng, lat }}*/}
+                {/*          radius={5}*/}
+                {/*          color={'#0000FF'}*/}
+                {/*        >*/}
+                {/*          <Popup>*/}
+                {/*            Lng: {lng}, Lat: {lat} <br/> {comment}*/}
+                {/*          </Popup>*/}
+                {/*        </CircleMarker>*/}
+                {/*      )*/}
+                {/*    )*/}
+                {/*  }*/}
+                {/*</LayerGroup>*/}
+              </BaseMap>
+            </Col>
+          ))(range(0, numMaps))
+        }
+      </Row>
+    </Container>
+  );
 }
+
+DemoBaseMap.propTypes = {
+  BaseMap: PropTypes.element.isRequired,
+  initialViewport: PropTypes.object.isRequired,
+  markers: PropTypes.array,
+  numMaps: PropTypes.number,
+};
+
+DemoBaseMap.defaultProps = {
+  numMaps: 1,
+  markers: [],
+};
+
+export default DemoBaseMap;
+
