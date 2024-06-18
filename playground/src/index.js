ea57ceabd34703a532897ed1c6bf92a4692ac853
rod-glover <rod.glover.ca@gmail.com>
2024-06-18 15:09:19 -0700

    Use createRoot to render playground app

--- playground/src/index.js
+++ playground/src/index.js
@@ -4,12 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createRoot} from "react-dom/client";
 
-ReactDOM.render(
+let container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
