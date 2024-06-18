e85228a7d2ab8c22f67b66280a5ae6f618b05db2
rod-glover <rod.glover.ca@gmail.com>
2024-06-18 15:09:55 -0700

    DemoStaticControl WIP

--- playground/src/DemoStaticControl.js
+++ playground/src/DemoStaticControl.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Col, Form, Row } from 'react-bootstrap';
-import { MapContainer } from 'react-leaflet';
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
 
 import { StaticControl } from 'pcic-react-leaflet-components';
 
@@ -14,6 +14,63 @@ function DemoStaticControl() {
   const handleChangeContent = e => {
     setContent(e.target.value)
   };
+
+  return (
+      <React.Fragment>
+        <Row>
+          <Col xs={6}>
+            <Form>
+              <Form.Group>
+                <Form.Label>Position</Form.Label>
+                <Form.Select value={position} onChange={handleChangePosition}>
+                  {
+                    "topleft topright bottomleft bottomright".split(" ").map(
+                        pos => (<option value={pos}>{pos}</option>)
+                    )
+                  }
+                </Form.Select>
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col xs={6}>
+            <Form>
+              <Form.Group>
+                <Form.Label>Content</Form.Label>
+                <Form.Control value={content} onChange={handleChangeContent}/>
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+        <Row className={"mt-3"}>
+          <Col>
+            Map goes here
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+              <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[51.505, -0.09]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+              <StaticControl position={position}>{content}</StaticControl>
+            </MapContainer>
+          </Col>
+        </Row>
+      </React.Fragment>
+  )
+
+
+  return (
+      <>
+          <div style={{ height: 80, border: "1px solid red" }}>
+            DemoStaticControl
+          </div>
+      </>
+  )
+
+  return "DemoStaticControl"
 
   return (
     <React.Fragment>
