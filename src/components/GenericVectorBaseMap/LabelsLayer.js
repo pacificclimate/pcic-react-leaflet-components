15c4149aaa2ebb8d4e5191c2f5b4b6b35c6b0987
Quintin <quintins@uvic.ca>
2024-09-05 11:05:23 -0700

    Add zIndex to labels layer

--- src/components/GenericVectorBaseMap/LabelsLayer.js
+++ src/components/GenericVectorBaseMap/LabelsLayer.js
@@ -6,6 +6,10 @@ const LabelsLayer = ({ wmsUrl, wmsOptions }) => {
     const map = useMap();
 
     useEffect(() => {
+        const legendName = process.env.REACT_APP_LEGEND_TYPE;
+        if (legendName == "achromatic") {
+            wmsOptions = { ...wmsOptions, zIndex: 100 }; // Display labels above all other layers.
+        }
         const wmsLayer = L.tileLayer.wms(wmsUrl, wmsOptions).addTo(map);
         return () => {
             map.removeLayer(wmsLayer);
